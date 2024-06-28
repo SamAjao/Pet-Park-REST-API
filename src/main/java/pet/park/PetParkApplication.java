32b20ad83ae5f64cf100072e463a5c2111bf32cf
unknown <samuel.ajao@gmail.com>
2024-06-28 14:12:24 -0700

    first commit`

---
+++ src/main/java/pet/park/PetParkApplication.java
@@ -0,0 +1,21 @@
+/*
+// Program: Promineo Tech Backend Java Back End Development Course
+// Author:  Samuel Ajao
+// Subject:  Spring Boot API Project - Pet Park
+// Create Date: June 27, 2024
+//
+*/
+package pet.park;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetParkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetParkApplication.class, args);
+
+	}
+
+}
---
+++ src/main/java/pet/park/entity/GeoLocation.java
@@ -0,0 +1,20 @@
+package pet.park.entity;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Data
+@NoArgsConstructor
+public class GeoLocation {
+	private BigDecimal latitude;
+	private BigDecimal longitude;
+	
+	GeoLocation(GeoLocation getLocation){
+		this.latitude = getLocation.latitude;
+		this.longitude = getLocation.longitude;
+	}
+}
